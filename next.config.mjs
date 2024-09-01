f3781f488d83fe89df7407deee3dd5e61675ef9d
Raslen <raslen.korbosli01@gmail.com>
2024-09-01 19:38:41 +0100

    fixing bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  future: { webpack5: true },
-
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  webpack: (config) => {
     config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
+
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
