3e8dcb97801143ea21cb212a31d748aad8b66640
Raslen korbosli <160639010+RaslenKorbosli@users.noreply.github.com>
2024-09-01 19:56:08 +0100

    Update PDF.ts

--- src/PDF.ts
+++ src/PDF.ts
@@ -1,7 +1,3 @@
 'use client';
 import { pdfjs } from 'react-pdf';
 
-('use client');
-
-pdfjs.GlobalWorkerOptions.workerSrc =
-  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js';
