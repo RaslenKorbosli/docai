05f5d47d5f56450329ea46ff2c36699e0c4346f0
Raslen <raslen.korbosli01@gmail.com>
2024-09-01 19:52:59 +0100

    bug

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
-
+    config.externals = [...config.externals, 'canvas', 'jsdom'];
     return config;
   },
 };
