bbac52441172cd79c52125dd1835ea362f113c9f
Raslen <raslen.korbosli01@gmail.com>
2024-09-02 22:05:54 +0100

    bug

--- src/PDF.ts
+++ src/PDF.ts
@@ -1,7 +1,1 @@
-'use client';
 import { pdfjs } from 'react-pdf';
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/legacy/build/pdf.worker.min.mjs',
-  import.meta.url
-).toString();
