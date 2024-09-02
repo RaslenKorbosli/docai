e32236cf0bdca18845fced0dbaca4591547656d0
Raslen korbosli <160639010+RaslenKorbosli@users.noreply.github.com>
2024-09-02 20:14:58 +0100

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,35 +1,5 @@
-import ChatForm from '@/components/ChatForm';
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from '@/components/ui/resizable';
-import { currentUser } from '@clerk/nextjs/server';
-import { Id } from '../../../convex/_generated/dataModel';
-
-import ChatMessages from '@/components/ChatMessages';
+import { redirect } from 'next/navigation';
 
 export default async function DashboardPage() {
-  const user = await currentUser();
-  if (!user) throw new Error('you must be logged in');
-
-  return (
-    <ResizablePanelGroup
-      direction="horizontal"
-      className="min-h-[calc(100vh-56px)] max-w-screen  border"
-    >
-      <ResizablePanel defaultSize={70}>
-        <div className="flex h-full items-center justify-center p-6">
-          <span className="font-semibold">Header</span>
-        </div>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={30} className="min-w-[300px] ">
-        <div className="flex flex-col  h-full  justify-between p-4 whitespace-pre-line ">
-          <ChatMessages />
-          <ChatForm userId={user.id as Id<'users'>} />
-        </div>
-      </ResizablePanel>
-    </ResizablePanelGroup>
-  );
+  return redirect('/dashboard/documents');
 }
